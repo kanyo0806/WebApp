8a509c62844dff200859ba4c51de54f88a9bb643
LChauke <92096848+kanyo0806@users.noreply.github.com>
2022-03-06 10:44:33 -0800

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using WebApplication.Models;
+using System.Data.Entity;
+
+namespace WebApplication
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //Initialize details database
+            //tabase.SetInitializer(new DetailsDatabaseInitializer());
+            Database.SetInitializer<DetailsContext>(new DropCreateDatabaseIfModelChanges<DetailsContext>());
+            
+        }
+    }
+}
